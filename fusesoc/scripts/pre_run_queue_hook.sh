bdee685490c2561d60f8c23dc46bb07616aeba19
z <ruben.teixeira@tecnico.ulisboa.pt>
2024-07-19 19:41:51 +0100

    Update more files and removed some files temporarely

--- fusesoc/scripts/pre_run_queue_hook.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-
-# grab board + skip programing
-# use this only to wait for board to be idle
-python3 ./software/python/board_client.py grab 600 -p 'echo dummy program command' -c 'echo dummy console command'
